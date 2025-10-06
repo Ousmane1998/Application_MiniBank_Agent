dbab63a05300e0834ed9fd6d87acebb6ea636d49
Ousmane1998 <papeousmanefaye011@gmail.com>
2025-10-06 17:00:24 +0000

    Ajout des guillemets

--- transfert-money-temp/src/services/userService.js
+++ transfert-money-temp/src/services/userService.js
@@ -12,19 +12,19 @@ export const createUtilisateur = async (data) => {
 
 
 export const updateUser = (id, data) =>
-  axios.put("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}", data);
+  axios.put(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}`, data);
 
 export const archiveUser = (id) =>
-  axios.patch("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}/archive");
+  axios.patch(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}/archive`);
 
 export const blockUser = (id) =>
-  axios.patch("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}/block");
+  axios.patch(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}/block`);
 export const unblockUser = (id) =>
-  axios.put("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}", { is_blocking: false });
+  axios.put(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}`, { is_blocking: false });
 export const unarchiveUser = (id) =>
-  axios.put("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}", { is_archived: false });
+  axios.put(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}`, { is_archived: false });
 export const deleteUser = (id) =>
-  axios.delete("https://application-minibank-agent.onrender.com/api/utilisateurs/${id}");
+  axios.delete(`https://application-minibank-agent.onrender.com/api/utilisateurs/${id}`);
 
 
 
